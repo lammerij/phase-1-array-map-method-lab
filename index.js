7fb7d5ea074b2424f8198a7ee345c260d7fede74
+ <+>
2022-02-14 12:09:38 -0500

    commit tree 7777f248ac2bf27c0bd0fc69874438e3b57d8c80

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+const tutorial = tutorials.map(tutorial => tutorial.split(' ').map(word =>{
+  return word[0].toUpperCase()+ word.slice(1)
+}).join(' ')
+)
+  return tutorial;
+};
+
+// pull a string from the array
+// isolate the first letter in each word
+// jsfiddle
